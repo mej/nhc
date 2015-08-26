cf137cbbae571badde1402f43c380104cf282bd7
Michael Jennings <mej@lbl.gov>
2015-08-25 22:02:34 -0700

    Renaming Warewulf -> LBNL

--- test/shut.inc.sh
+++ test/shut.inc.sh
@@ -7,10 +7,9 @@
 #
 
 # This is a simple unit test framework for BASH shell scripts.  The
-# interface is based on Perl's Test::More which is used by Warewulf.
-# SHUT stands for "SHell Unit Testing."  I almost named it the Shell
-# Testing Framework for Unix (STFU), but that seemed to make others
-# inexplicably angry....
+# interface is based on Perl's Test::More.  SHUT stands for "SHell
+# Unit Testing."  I almost named it the Shell Testing Framework for
+# Unix (STFU), but that seemed to make others inexplicably angry....
 
 SHUT_SKIPPING=0
 SHUT_CHECK_SKIP='if skipping ; then skip ; return 0 ; fi'
